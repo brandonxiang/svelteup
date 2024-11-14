443a0bbc599f63db4c0de988855162f0081441cd
brandon.xiang <1542453460@qq.com>
2024-11-14 19:26:32 +0800

    chore: update eslint version

--- examples/custom-element-split/components/counter-app.svelte
+++ examples/custom-element-split/components/counter-app.svelte
@@ -2,7 +2,11 @@
 
 <script>
   let count = $state(0);
+
+  function increment() {
+    count += 1;
+  }
 </script>
 
 <input type="number" bind:value={count} />
-<button onclick={() => (count += 1)}>count</button>
+<button onclick={increment}>count</button>
